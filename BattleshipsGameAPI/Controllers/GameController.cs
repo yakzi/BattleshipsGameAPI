6994cca610efbcc49c4530a0933305f4f9347833
yakzi <zieba.jakub.97@gmail.com>
2022-01-10 21:35:28 +0100

    ADD some basic methods to store player board in a json file - WIP

--- BattleshipsGameAPI/Controllers/GameController.cs
+++ BattleshipsGameAPI/Controllers/GameController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BattleshipsGameDotNET.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BattleshipsGameAPI.Controllers
 {
     public class GameController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("CreatePlayer")]
+        public void CreatePlayer(string Name)
         {
-            return View();
+            Player player = new Player();
+            CreateBoard(player);
+            player.SaveBoard(player.Board, @$"C:\Users\jakub\Desktop\TET\test{Name}.json");
+        }
+        private void CreateBoard(Player player)
+        {
+            player.Board = new List<Point>();                              //Basic board, with all fields set to empty
+            for (var x = 0; x < 10; x++)
+            {
+                for (var y = 0; y < 10; y++)
+                {
+                    player.Board.Add(new Point
+                    {
+                        Field = Field.Empty,
+                        X = x,
+                        Y = y
+                    });
+                }
+            }
+        }
+        [HttpPost]
+        [Route("GetBoard/{id}")]
+        public List<Point> GetBoard(Player player)
+        {
+           return player.LoadBoard(@"C:\Users\jakub\Desktop\TET\test0.json");
         }
     }
 }
--- BattleshipsGameAPI/Models/Player.cs
+++ BattleshipsGameAPI/Models/Player.cs
@@ -1,9 +1,19 @@
-﻿namespace BattleshipsGameDotNET.Models
+﻿using Newtonsoft.Json;
+
+namespace BattleshipsGameDotNET.Models
 {
     public class Player
     {
         public int Score;
         public List<Point> Hits = new List<Point>();
         public List<Point> Board { get; set; }
+        public List<Point> LoadBoard(string Path)
+        {
+            return JsonConvert.DeserializeObject<List<Point>>(File.ReadAllText(Path));
+        }
+        public void SaveBoard(List<Point> Board, string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(Board));
+        }
     }
 }
