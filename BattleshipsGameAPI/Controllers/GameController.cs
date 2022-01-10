12f1bdaf9474141f4755d3333fb05f12b1085e00
yakzi <zieba.jakub.97@gmail.com>
2022-01-10 21:08:24 +0100

    Dodaj pliki projektów.

---
+++ BattleshipsGameAPI/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BattleshipsGameAPI.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
