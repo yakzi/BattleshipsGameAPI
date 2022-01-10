0ae906620ad2f0d91a2ff45622f3f5ec10e87fe0
yakzi <zieba.jakub.97@gmail.com>
2022-01-10 22:07:34 +0100

    ADD ControllerBase class to handle JSON operations

---
+++ BattleshipsGameAPI/Controllers/ControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using BattleshipsGameDotNET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BattleshipsGameAPI.Controllers
+{
+    public class ControllerBase : Controller
+    {
+        public List<Point> LoadBoard(string Path)
+        {
+            return JsonConvert.DeserializeObject<List<Point>>(System.IO.File.ReadAllText(Path));
+        }
+        public void SaveBoard(List<Point> Board, string path)
+        {
+            System.IO.File.WriteAllText(path, JsonConvert.SerializeObject(Board));
+        }
+    }
+}
