b9228f3e2d501515aaac02f8ab67fc22c41745b5
yakzi <zieba.jakub.97@gmail.com>
2022-01-13 18:02:02 +0100

    ADD Player repository, json player repository (for path purpose)

---
+++ BattleshipsGameAPI/Services/Options/JsonPlayerRepositoryOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace BattleshipsGameAPI.Services.Options
+{
+    public class JsonPlayerRepositoryOptions
+    {
+        public string StorageBasePath { get; init; }
+    }
+}
---
+++ BattleshipsGameAPI/Services/PlayerRepository.cs
@@ -0,0 +1,34 @@
+﻿using BattleshipsGameAPI.Services.Options;
+using BattleshipsGameDotNET.Models;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+    public class PlayerRepository
+    {
+        public interface IPlayerRepository
+        {
+            Task<Player> LoadPlayerAsync(string playerId, CancellationToken cancellationToken);
+            Task SavePlayerAsync(Player player, CancellationToken cancellationToken);
+        }
+
+        internal class JsonPlayerRepository : IPlayerRepository
+        {
+            private readonly JsonPlayerRepositoryOptions options;
+
+            public JsonPlayerRepository(IOptions<JsonPlayerRepositoryOptions> options)
+            {
+                this.options = options.Value;
+            }
+
+            public async Task<Player> LoadPlayerAsync(string playerId, CancellationToken cancellationToken)
+            {
+                var playerFilePath = Path.Combine(options.StorageBasePath, $"{playerId}.json");
+
+                if (!File.Exists(playerFilePath))
+                    return null;
+
+                return JsonConvert.DeserializeObject<Player>(await File.ReadAllTextAsync(playerFilePath, cancellationToken));
+            }
+            public async Task SavePlayerAsync(Player player, CancellationToken cancellationToken) =>
+                await File.WriteAllTextAsync(Path.Combine(options.StorageBasePath, $"{player.Id}.json"), JsonConvert.SerializeObject(player, Formatting.Indented), cancellationToken);
+        }
+    }
