59597ae354c462131e49311f764f12d2fe4937d1
yakzi <zieba.jakub.97@gmail.com>
2022-01-13 18:47:21 +0100

    ADD PlaceShipOnPlayerBoard records

---
+++ BattleshipsGameAPI/Controllers/Models/PlaceShipOnPlayerBoard.cs
@@ -0,0 +1,7 @@
+﻿using BattleshipsGameDotNET.Models;
+
+namespace BattleshipsGameAPI.Controllers.Models
+{
+    record PlaceShipOnPlayerBoardRequest(int shipLength, Direction direction, string playerId);
+    record PlaceShipOnPlayerBoardResponse(List<Point> Board);
+}
